1531e38ff4f1cb1407b69a5f0877c4be25d4c6a9
andreaTurr <a.turrini2@campus.unimib.it>
2022-10-16 12:54:02 +0200

    Add Comment

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+//commento 1
 public class MainActivity extends AppCompatActivity {
 
     @Override
