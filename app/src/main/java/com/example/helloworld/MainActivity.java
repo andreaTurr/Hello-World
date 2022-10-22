9ac39ab48b8dd7d1b161997acbf795c6389b8a46
andreaTurr <a.turrini2@campus.unimib.it>
2022-10-22 14:44:20 +0200

    secoda Lezione Esercitaione spiegazione tools android studio

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,18 +3,29 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 //commento 1
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //aggiunto commento da locale
-        //aggiunto commento da remoto 2
 
-        //commento da completare
-        //prova amend
-        //prova amend 2
+        Log.e(TAG, "onCreate()");
+
+        TextView textView = findViewById(R.id.hello_text);
+
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(view -> Log.e(TAG, "Bottone Premuto"));
+    }
+
+    public void sendMessage(View view){
     }
 }
